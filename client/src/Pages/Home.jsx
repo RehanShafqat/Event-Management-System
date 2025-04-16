23606abc147699fa41835611c353eea10ad7ec19
Aneeq Zafar <zafaraay@gmail.com>
2025-04-16 17:03:58 +0500

    client project setup

---
+++ client/src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/Button";
+const Home = () => {
+  return (
+    <div>
+      Home
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default Home;
