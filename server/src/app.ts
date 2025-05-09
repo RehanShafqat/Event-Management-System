2df5cac1f086b13b591b95520b55a2a2b78189de
Muhammad Rehan Shafqat <rehanshafqat2004@gmail.com>
2025-05-10 00:50:41 +0500

    swagger integrated

--- server/src/app.ts
+++ server/src/app.ts
@@ -6,6 +6,11 @@ import { errorHandler } from "./middleware/error.middleware";
 import redisClient from "./config/redis";
 import logger from "./utils/logger";
 import AuthRoutes from "./routes/auth.routes";
+import helmet from "helmet";
+import morgan from "morgan";
+import swaggerJSDoc from "swagger-jsdoc";
+import { swaggerOptions } from "./config/swagger";
+import swaggerUi from "swagger-ui-express";
 dotenv.config();
 
 const app = express();
@@ -13,7 +18,15 @@ const app = express();
 // Connect to MongoDB
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -30,6 +43,13 @@ connectDB().then(() => {
     logger.info(`Server is running on port localhost:${process.env.PORT}`);
   });
 });
+
+app.use(helmet());
+app.use(morgan("dev"));
+
+// Swagger documentation
+const specs = swaggerJSDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 // Routes
 app.use("/api/auth", AuthRoutes);
